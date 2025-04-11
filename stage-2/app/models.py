98e92c3d8b81dc95a88bf319493cb968e71f05a1
Mehreen Saghar <139617285+mehreensaghar@users.noreply.github.com>
2025-04-11 17:25:11 +0500

    Update models.py

--- stage-2/app/models.py
+++ stage-2/app/models.py
@@ -1,1 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class Store(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    sku = db.Column(db.String(50), unique=True)
+
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer, default=0)
+
+class StockMovement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'))
+    type = db.Column(db.String(10))  # 'stockin', 'sale', 'removal'
+    quantity = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, server_default=db.func.now())
 
