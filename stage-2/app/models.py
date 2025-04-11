7c7be11733e8a5b6eba1bbf048825e13dfba4572
Mehreen Saghar <139617285+mehreensaghar@users.noreply.github.com>
2025-04-11 17:47:06 +0500

    Update models.py

--- stage-2/app/models.py
+++ stage-2/app/models.py
@@ -1,25 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+
+db=SQLAlchemy()
 
 class Store(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    sku = db.Column(db.String(50), unique=True)
-
-class Inventory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
-    quantity = db.Column(db.Integer, default=0)
+    name = db.Column(db.String)
 
 class StockMovement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'))
-    type = db.Column(db.String(10))  # 'stockin', 'sale', 'removal'
+    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
     quantity = db.Column(db.Integer)
-    timestamp = db.Column(db.DateTime, server_default=db.func.now())
-
+    type = db.Column(db.String) 
+    timestamp = db.Column(db.DateTime)
